51f0a5666b39726bc86ec478500114392ab69497
behrooz <behrooz.zendedel1992@gmail.com>
2022-04-14 07:42:33 +0430

    add useeffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [error, setError] = useState(null);
   const [loading, setLoding] = useState(false);
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    setLoding(true);
+    setUser(null);
+    setError(null);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setLoding(false);
+        setUser(res.data);
+      })
+      .catch((err) => {
+        setLoding(false);
+        setError(err.message);
+      });
+  }, []);
+
   return (
     <div className="App">
       {loading && <p>loading ....</p>}
       {error && <p>{error}</p>}
-      {user && <div>user loaded!</div>}
+      {user && (
+        <div>
+          {user.map((user) => (
+            <li>{user.name}</li>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
